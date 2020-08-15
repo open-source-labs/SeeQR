3d83178e1e567d2f4dac47f3723cf98f43c2a431
Catherine <cc2368@cornell.edu>
2020-08-14 19:48:13 -0700

    added dev1 script to test

--- frontend/electron.js
+++ frontend/electron.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 
+
+const root = document.createElement("div");
+
+root.id = "root";
+document.body.appendChild(root);
+
 render(
   <div>
-    <h1>Hiii SeeQR</h1>
+    <h1>Hiii asdfasdfsadfsSeeQeR</h1>
     <App />
   </div>,
   document.getElementById('root')
