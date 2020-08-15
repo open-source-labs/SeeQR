5dfc21bb9ca6bcb8c5465bbcfa52b950e45ef97a
serenackuo <hello@serenakuo.com>
2020-08-14 22:45:37 -0400

    fix linking app component and hot reload in dev env

---
+++ frontend/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2>Some content here from app!</h2>
+    </div>
+  );
+};
+
+export default App;
--- frontend/electron.js
+++ frontend/electron.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 
+const root = document.createElement('div');
+root.id = 'root';
+document.body.appendChild(root);
+
 render(
   <div>
     <h1>Hiii SeeQR</h1>
