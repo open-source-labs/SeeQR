d0d193b1f26071c0b650cff872fbe0ceae1b3c4d
serenackuo <hello@serenakuo.com>
2020-08-17 15:31:31 -0400

    readd additional components splash and main

---
+++ frontend/components/Main.tsx
@@ -0,0 +1,8 @@
+// import React, { useEffect } from 'react';
+// import '../style/style.css';
+
+// const Main: React.FC = () => {
+//   return <div>This is the main menu</div>;
+// };
+
+// export default Main;
---
+++ frontend/components/Splash.tsx
@@ -0,0 +1,49 @@
+// import React, { useState, useEffect } from 'react';
+// import '../style/style.css';
+// import dialog from 'electron';
+
+// // for safetyping any react components.
+// export interface SplashProps {
+//   setOpenApp: React.Dispatch<React.SetStateAction<boolean>>;
+// }
+// // export a file obj in the state
+// export interface File {
+//   filePath: string;
+// }
+
+// const Splash: React.FC<SplashProps> = ({ setOpenApp }) => {
+//   const [file, setFile] = useState<File>();
+//   // a dialogue menu with retrieve the file path
+//   const getFile = async () => {
+//     dialog
+//       .showOpenDialog(
+//         {
+//           properties: ['openFile', 'multiSelections'],
+//         },
+//         (input: any) => {
+//           if (input !== undefined) {
+//             // handle files
+//             console.log('file path undefined');
+//           }
+//         }
+//       )
+//       .then((result: any) => {
+//         setFile(result);
+//         console.log('result', result);
+//       })
+//       .catch((err: any) => {
+//         console.log(err);
+//       });
+//   };
+//   // run dialogue menu
+//   useEffect(() => {
+//     getFile();
+//   }, []);
+//   return (
+//     <div>
+//       <h3>{file}</h3>
+//     </div>
+//   );
+// };
+
+// export default Splash;
