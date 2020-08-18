c164ff4f151c69b9979a353e2d6be6dfa44ea30d
Catherine <cc2368@cornell.edu>
2020-08-18 14:36:03 -0700

    commented out splash window

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, MouseEvent } from 'react';
 import { Splash } from './Splash';
 import MainPanel from './MainPanel';
 import '../assets/stylesheets/styles.css';
+
+const { dialog } = require('electron').remote;
+const { ipcRenderer } = window.require('electron');
+
+type ClickEvent = React.MouseEvent<HTMLElement>;
 
 type state = {
   openSplash: boolean;
@@ -12,20 +17,61 @@ type AppProps = {};
 export class App extends Component<AppProps, state> {
   constructor(props: AppProps) {
     super(props);
+    this.handleFileClick = this.handleFileClick.bind(this);
+    this.handleSkipClick = this.handleSkipClick.bind(this);
   }
   state: state = {
     openSplash: true,
     //files: [],
   };
 
+  handleFileClick(event: ClickEvent) {
+    // event.preventDefault();
+    // alert('event triggered');
+    const options = {
+      filters: [{ name: 'All Files', extensions: ['*'] }],
+    };
+    dialog
+      .showOpenDialog(
+        {
+          properties: ['openFile', 'multiSelections'],
+        },
+        (files: string) => {
+          if (files !== undefined) {
+            // handle files
+            console.log('file path undefined');
+          }
+        }
+      )
+      .then((result: object) => {
+        // there is definitely a better way to reference the object key of filePaths
+        const filePathArr = Object.values(result)[1];
+        // send via channel to main process
+        ipcRenderer.send('database-file-submission', filePathArr)
+      })
+      .catch((err: object) => {
+        console.log(err);
+      });
+  }
+
+  handleSkipClick(event: ClickEvent) {
+    ipcRenderer.send('skip-file-upload');
+    this.setState({ openSplash: false });
+  }
+
+
   render() {
     return (
       <div>
         {this.state.openSplash ? (
-          <Splash openSplash={this.state.openSplash} />
+          <Splash
+            openSplash={this.state.openSplash}
+            handleFileClick={this.handleFileClick}
+            handleSkipClick={this.handleSkipClick}
+          />
         ) : (
-          <MainPanel />
-        )}
+            <MainPanel />
+          )}
       </div>
     );
   }
