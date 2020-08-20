8333890adbe5a4542b8d3635c66be72564b908a9
Muhammad Trad <muhammad.trad@yahoo.com>
2020-08-19 17:14:55 -0700

    add child process

--- backend/controllers.js
+++ backend/controllers.js
@@ -2,7 +2,7 @@ const database = {};
 const { exec } = require("child_process");
 
 database.testing = () => {
-    exec("docker container attach postgres-1", (error, stdout, stderr) => {
+    exec('docker exec postgres-1 psql -h localhost -p 5432 -U postgres -c "\\c hello_there;"', (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             return;
