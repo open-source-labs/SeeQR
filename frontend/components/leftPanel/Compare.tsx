c24ab9b82709d918c684ac882f99f72a99f3217a
Muhammad Trad <muhammad.trad@yahoo.com>
2020-08-30 21:27:38 -0700

    add compare query remove

--- frontend/components/leftPanel/Compare.tsx
+++ frontend/components/leftPanel/Compare.tsx
@@ -18,7 +18,27 @@ export const Compare = (props: CompareProps) => {
   let initial: any = {...props, compareList: [] };
   const [ queryInfo, setCompare ] = useState(initial);
 
-  const renderCompare = () => {
+  const addCompareQuery = (event) => {
+    let compareList = queryInfo.compareList;
+    props.queries.forEach((query) => {
+      if (query.queryLabel === event.target.text){
+        compareList.push(query);
+      }
+    });
+    setCompare({...queryInfo, compareList});
+  }
+
+  const deleteCompareQuery = (event) => {
+    let compareList: any = queryInfo.compareList.filter(
+      (query) => query.queryLabel !== event.target.id);
+      setCompare({...queryInfo, compareList});
+    }
+
+    const dropDownList = () => {
+      return props.queries.map((query, index) => <Dropdown.Item id={`query-item${index}`} className="queryItem" onClick={addCompareQuery}>{query.queryLabel}</Dropdown.Item>);
+    };
+
+    const renderCompare = () => {
     return queryInfo.compareList.map((query, index) => {
       const { queryStatistics, querySchema, queryLabel } = query;
       const { ["QUERY PLAN"]: queryPlan } = queryStatistics[0];
@@ -38,29 +58,16 @@ export const Compare = (props: CompareProps) => {
           <td id="schema-name">{querySchema}</td>
           <td id="actual-rows">{actualRows}</td>
           <td id="total-time">{actualTotalTime}</td>
+          <button id={queryLabel} className="delete-query-button" onClick={deleteCompareQuery}>X</button>
         </tr>
       );
     });
   };
 
-  const addCompareQuery = (event) => {
-    let compareList = queryInfo.compareList;
-    props.queries.forEach((query) => {
-      if (query.queryLabel === event.target.text){
-        compareList.push(query);
-      }
-    });
-    setCompare({...queryInfo, compareList});
-  }
-
-  const dropDownList = () => {
-    return props.queries.map((query, index) => <Dropdown.Item id={`query-item${index}`} className="queryItem" onClick={addCompareQuery}>{query.queryLabel}</Dropdown.Item>);
-  };
-
   return (
     <div id="compare-panel">
       <h3>Compare</h3>
-        <DropdownButton id="add-query-button" title="Add Query">
+        <DropdownButton id="add-query-button" title="Add Query Data">
           {dropDownList()}
         </DropdownButton>
         <table className="compare-box">
