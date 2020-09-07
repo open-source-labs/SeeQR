95f805a0e927edb8bab1d700a8fd8e9a88431d73
Catherine <cc2368@cornell.edu>
2020-09-07 02:52:34 -0700

    refactor require statements in Query component to import statement for CodeMirror dependency

--- frontend/components/rightPanel/schemaChildren/Query.tsx
+++ frontend/components/rightPanel/schemaChildren/Query.tsx
@@ -3,22 +3,11 @@ import React, { Component } from 'react';
 const { ipcRenderer } = window.require('electron');
 const { dialog } = require('electron').remote;
 
-// Codemirror Styling
-require('codemirror/lib/codemirror.css');
-
-// Codemirror Languages
-require('codemirror/mode/javascript/javascript');
-require('codemirror/mode/sql/sql');
-
-// Codemirror Themes
-require('codemirror/mode/markdown/markdown');
-require('codemirror/theme/monokai.css');
-require('codemirror/theme/midnight.css');
-require('codemirror/theme/lesser-dark.css');
-require('codemirror/theme/solarized.css');
-
-// Codemirror Component
-var CodeMirror = require('react-codemirror');
+// Codemirror configuration
+import 'codemirror/lib/codemirror.css'; // Styline
+import 'codemirror/mode/sql/sql'; // Language (Syntax Highlighting)
+import 'codemirror/theme/lesser-dark.css'; // Theme
+import CodeMirror from 'react-codemirror';
 
 /************************************************************
  *********************** TYPESCRIPT: TYPES ***********************
