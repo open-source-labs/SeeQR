a142496481fde451e7f2110b07abb5a09426de65
Casey Walker <casey.e.walker@gmail.com>
2020-10-01 20:05:51 -0700

    slightly more than boilerplate

--- frontend/components/rightPanel/schemaChildren/DummyDataModal.tsx
+++ frontend/components/rightPanel/schemaChildren/DummyDataModal.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const { dialog } = require('electron').remote;
 const { ipcRenderer } = window.require('electron');
@@ -9,6 +11,7 @@ type DummyDataModalProps = {
     show: boolean;
     showModal: any;
     onClose: any;
+    schemaTables: string[];
 };
 
 type state = {
@@ -17,6 +20,7 @@ type state = {
 }
 
 class DummyDataModal extends Component<DummyDataModalProps, state> {
+
   constructor(props: DummyDataModalProps) {
     super(props);
   }
@@ -25,14 +29,26 @@ class DummyDataModal extends Component<DummyDataModalProps, state> {
     dataInfo : {}
   }
 
+  dropDownList = () => {
+    //returns list of table names from schema
+      //untested code:
+      return this.props.schemaTables.map((tableName, index) => <Dropdown.Item></Dropdown.Item>)
+  };
+
   render() {
+
     if (this.props.show === false) {
       return null;
     }
+
     return (
-      <div>
+      <div className="dummy-data-modal">
+        <h3>Generate Dummy Data</h3>
+        <p>select table</p>
 
       </div>
     )
   }
 }
+
+export default DummyDataModal;
