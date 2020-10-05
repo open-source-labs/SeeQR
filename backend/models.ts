fa9c4f0fb24dfe2e88edf8ef4459bc82cdbb59bd
Justin Dury-Agri <justin.dury.agri@gmail.com>
2020-10-05 14:07:36 -0700

    Pseudocode for dummyData generator, connected getSchemaLayout from front-end to back-end.

--- backend/models.ts
+++ backend/models.ts
@@ -46,4 +46,45 @@ module.exports = {
         });
     });
   },
+  getSchemaLayout: () => {
+    // initialize a new promise; we resolve this promise at the end of the last async function within the promise
+    return new Promise((resolve) => {
+      const schemaLayout: any = {
+        tableNames: [],
+        tables: {
+          // tableName: [columnNames array]
+        }
+      };
+      pool
+        // This query returns the names of all the tables in the database
+        .query(
+          "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name;"
+        )
+        // then we save the table names into the schemaLayout object in the tableNames property
+        .then((tables) => {
+          for (let i = 0; i < tables.rows.length; ++i) {
+            schemaLayout.tableNames.push(tables.rows[i].table_name);
+          }
+        })
+        // then we iterate over the tableNames we just got and run a query on each to get an array of column names
+        .then(() => {
+          for (const tableName of schemaLayout.tableNames){
+            // This query returns the names of all the columns in the specified table
+            const queryString = "SELECT column_name FROM information_schema.columns WHERE table_name = $1;";
+            const value = [tableName];
+            pool
+              .query(queryString, value)
+              .then((result) => {
+                schemaLayout.tables[tableName] = [];
+                for (let i = 0; i < result.rows.length; ++i) {
+                  schemaLayout.tables[tableName].push(result.rows[i].column_name);
+                }
+              });
+          }
+          // we resolve schemaLayout so that we know we have a resolved promise for whatever invoked getSchemaLayout
+          resolve(schemaLayout);
+          }
+      )
+    });
+  }
 };
