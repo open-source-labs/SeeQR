7cfad48a6c7c382b4ca0aaa3e2d9753070ba58fc
Jennifer Courtner <jmichele.courtner@gmail.com>
2021-01-05 13:05:10 -0500

    modification to server and queryController.

--- backend/controllers/queryController.ts
+++ backend/controllers/queryController.ts
@@ -2,78 +2,98 @@ const db = require('../models');
 
 const queryController = {
   executeQueryUntracked: (req, res, next) => {
-  // event.sender.send('async-started');
+    // event.sender.send('async-started');
 
-  // destructure object from frontend
-  const { queryString } = req.body;
-  // run query on db
-  db.query(queryString)
-    .then(() => {
-      (async function getListAsync() {
-        let listObj = await db.getLists();
-        // event.sender.send('db-lists', listObj);
-        // event.sender.send('async-complete');
-      })();
-    }).then(next())
-    .catch((error: string) => {
-      // event.sender.send('query-error', 'Error executing query.');
-    });
-    
+    // destructure object from frontend
+    const { queryString } = req.body;
+    // run query on db
+    db.query(queryString)
+      .then(() => {
+        (async function getListAsync() {
+          let listObj = await db.getLists();
+          // event.sender.send('db-lists', listObj);
+          // event.sender.send('async-complete');
+        })();
+      })
+      .then(next())
+      .catch((error: string) => {
+        // event.sender.send('query-error', 'Error executing query.');
+      });
   },
 
   executeQueryTracked: (req, res, next) => {
+    // create data object to send back to client
+    // const frontendData = {
+    //   queryData: null,
+    //   queryStats: null,
+    // };
+    // // extract query string from client request
+    // const { queryString } = req.body;
+    // // match the connection pool based on cookies
+    // const pool = users[req.cookies['session_id']];
+    // // retrieve query results and attach to frontend data object
+    // const rows = await pool.query(queryString);
+    // frontendData.queryData = rows;
+    // // Run EXPLAIN (FORMAT JSON, ANALYZE)
+    // if (!queryString.match(/create/i)) {
+    //   const queryStats = await pool.query(
+    //     'EXPLAIN (FORMAT JSON, ANALYZE) ' + queryString
+    //   );
+    //   frontendData.queryStats = queryStats;
+    // }
+    // // send back to client
+    // return res.status(200).json(frontendData);
+
+    /*
+      event.sender.send('async-started');
     
-  //   console.log('hello world')
-  // // send notice to front end that query has been started
-  // // event.sender.send('async-started');
+      // destructure object from frontend
+      const { queryString, queryCurrentSchema, queryLabel } = data;
+    
+      // initialize object to store all data to send to frontend
+      let frontendData = {
+        queryString,
+        queryCurrentSchema,
+        queryLabel,
+        queryData: '',
+        queryStatistics: '',
+        lists: {},
+      };
+    
+      // Run select * from actors;
+      db.query(queryString)
+        .then((queryData) => {
+          frontendData.queryData = queryData.rows;
+          if (!queryString.match(/create/i)) {
+            // Run EXPLAIN (FORMAT JSON, ANALYZE)
+            db.query('EXPLAIN (FORMAT JSON, ANALYZE) ' + queryString).then(
+              (queryStats) => {
+                frontendData.queryStatistics = queryStats.rows;
+    
+                (async function getListAsync() {
+                  listObj = await db.getLists();
+                  frontendData.lists = listObj;
+                  event.sender.send('db-lists', listObj);
+                  event.sender.send('return-execute-query', frontendData);
+                  event.sender.send('async-complete');
+                })();
+              }
+            );
+          } else {
+            // Handling for tracking a create table query, can't run explain/analyze on create statements
+            (async function getListAsync() {
+              listObj = await db.getLists();
+              frontendData.lists = listObj;
+              event.sender.send('db-lists', listObj);
+              event.sender.send('async-complete');
+            })();
+          }
+        })
+        .catch((error: string) => {
+          console.log('ERROR in execute-query-tracked channel in main.ts', error);
+        });
+        */
 
-  // // destructure object from frontend
-  // const { queryString, queryCurrentSchema, queryLabel } = req.body;
-  // console.log('query string: ', queryString);
-  // console.log('request body ', req.body);
-
-  // // initialize object to store all data to send to frontend
-  // let frontendData = {
-  //   queryString,
-  //   queryCurrentSchema,
-  //   queryLabel,
-  //   queryData: '',
-  //   queryStatistics: '',
-  //   lists: {},
-  // };
-
-  // // Run select * from actors;
-  // db.query(queryString)
-  //   .then((queryData) => {
-  //     frontendData.queryData = queryData.rows;
-  //     if (!queryString.match(/create/i)) {
-  //       // Run EXPLAIN (FORMAT JSON, ANALYZE)
-  //       db.query('EXPLAIN (FORMAT JSON, ANALYZE) ' + queryString).then(
-  //         (queryStats) => {
-  //           frontendData.queryStatistics = queryStats.rows;
-
-  //           (async function getListAsync() {
-  //             let listObj = await db.getLists();
-  //             frontendData.lists = listObj;
-  //             // event.sender.send('db-lists', listObj);
-  //             // event.sender.send('return-execute-query', frontendData);
-  //             // event.sender.send('async-complete');
-  //           })();
-  //         }
-  //       );
-  //     } else {
-  //       // Handling for tracking a create table query, can't run explain/analyze on create statements
-  //       (async function getListAsync() {
-  //         let listObj = await db.getLists();
-  //         frontendData.lists = listObj;
-  //         // event.sender.send('db-lists', listObj);
-  //         // event.sender.send('async-complete');
-  //       })();
-  //     }
-  //   }).then(next())
-  //   .catch((error: string) => {
-  //     console.log('ERROR in execute-query-tracked channel in main.ts', error);
-  //   });
     next();
   },
   generateDummyData: (req, res, next) => {
--- backend/server.ts
+++ backend/server.ts
@@ -24,19 +24,23 @@ server.get('/', makeDB, (req, res) => {
 server.use(express.static('dist'));
 
 async function makeDB(req, res, next) {
-  if(!('session_id' in req.cookies)) {
+  if (!('session_id' in req.cookies)) {
     const options = {
       method: 'POST',
-      headers: { 
-        'Authorization': 'Basic Ojg4MDVmN2U2LTBiZWUtNDcwNC04OWRlLTU5YmM2ZTJlNWEyYw==',
-      }
-    }
-    const response = await fetch(`https://customer.elephantsql.com/api/instances?name=db${++dbnum}9&plan=turtle&region=amazon-web-services::us-east-1`, options);
+      headers: {
+        Authorization:
+          'Basic Ojg4MDVmN2U2LTBiZWUtNDcwNC04OWRlLTU5YmM2ZTJlNWEyYw==',
+      },
+    };
+    const response = await fetch(
+      `https://customer.elephantsql.com/api/instances?name=db${++dbnum}9&plan=turtle&region=amazon-web-services::us-east-1`,
+      options
+    );
     const data = await response.json();
     const { id, url } = data;
-    const expiry = 300000;
+    const expiry = 1200000;
     users[id] = new Pool({ connectionString: url });
-    res.cookie('session_id', id, { maxAge: expiry});
+    res.cookie('session_id', id, { maxAge: expiry });
     setTimeout(() => deleteDB(id), expiry); //1 minute
   }
   next();
@@ -45,14 +49,17 @@ async function makeDB(req, res, next) {
 async function deleteDB(id) {
   const options = {
     method: 'DELETE',
-    headers: { 
-      'Authorization': 'Basic Ojg4MDVmN2U2LTBiZWUtNDcwNC04OWRlLTU5YmM2ZTJlNWEyYw==',
-    }
-  }
-  const response = await fetch(`https://customer.elephantsql.com/api/instances/${id}`, options);
+    headers: {
+      Authorization:
+        'Basic Ojg4MDVmN2U2LTBiZWUtNDcwNC04OWRlLTU5YmM2ZTJlNWEyYw==',
+    },
+  };
+  const response = await fetch(
+    `https://customer.elephantsql.com/api/instances/${id}`,
+    options
+  );
   console.log(response.status);
 }
-
 
 // let static middleware do its job
 // server.use(express.static(__dirname + '/public'));
@@ -66,12 +73,14 @@ async function deleteDB(id) {
 // router for 'execute-query-untracked', 'execute-query-tracked', 'generate-dummy-data'
 server.put('/query/execute-query-tracked', async (req, res, next) => {
   // create data object to send back to client
+
+  console.log(req.body);
   const frontendData = {
     queryData: null,
     queryStats: null,
   };
   // extract query string from client request
-  const { queryString }  = req.body;
+  const { queryString } = req.body;
   // match the connection pool based on cookies
   const pool = users[req.cookies['session_id']];
   // retrieve query results and attach to frontend data object
@@ -79,9 +88,11 @@ server.put('/query/execute-query-tracked', async (req, res, next) => {
   frontendData.queryData = rows;
   // Run EXPLAIN (FORMAT JSON, ANALYZE)
   if (!queryString.match(/create/i)) {
-    const queryStats = await pool.query('EXPLAIN (FORMAT JSON, ANALYZE) ' + queryString);
-    frontendData.queryStats = queryStats;
-  } 
+    const queryStats = await pool.query(
+      'EXPLAIN (FORMAT JSON, ANALYZE) ' + queryString
+    );
+    frontendData.queryStats = queryStats.rows;
+  }
   // send back to client
   return res.status(200).json(frontendData);
 });
