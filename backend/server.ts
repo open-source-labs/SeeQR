04457fdeaa362b96857fa134f9fcdd20abfae4e4
chrisak47 <72534276+chrisak47@users.noreply.github.com>
2021-01-05 12:58:14 -0500

    tweaked the cookies and sessions

--- backend/server.ts
+++ backend/server.ts
@@ -34,10 +34,21 @@ async function makeDB(req, res, next) {
     const response = await fetch(`https://customer.elephantsql.com/api/instances?name=db${++dbnum}9&plan=turtle&region=amazon-web-services::us-east-1`, options);
     const data = await response.json();
     const { id, url } = data;
-    const expiry = 300000;
+    const expiry = 1200000;
     users[id] = new Pool({ connectionString: url });
     res.cookie('session_id', id, { maxAge: expiry});
-    setTimeout(() => deleteDB(id), expiry); //1 minute
+    setTimeout(() => deleteDB(id), expiry); //20 minutes
+  } else {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Basic Ojg4MDVmN2U2LTBiZWUtNDcwNC04OWRlLTU5YmM2ZTJlNWEyYw==',
+      }
+    }
+    const response = await fetch(`https://customer.elephantsql.com/api/instances/${req.cookies.session_id}`, options);
+    const data = await response.json();
+    const { url } = data;
+    users[req.cookies.session_id] = new Pool({ connectionString: url });
   }
   next();
 }
