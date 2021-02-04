cc244678abe778f5ae98bb1819525988e6bd549f
Faraz Akhtar <faraz@Farazs-Air.fios-router.home>
2021-02-03 20:08:00 -0500

    added add comments

--- backend/models.ts
+++ backend/models.ts
@@ -255,14 +255,19 @@ myobj = {
       return Promise.resolve(pool.query(queryString));
     };
 
-    // iterate over tables, running drop queries, and pushing a new promise to promise array
-    for (const table in keyObject) {
-      await generateAndRunDropQuery(table);
+    const arrayOfObj = Object.keys(keyObject);
+    for (let i = 0; i < arrayOfObj.length; i += 1) {
+      await generateAndRunDropQuery(i);
     }
+
+    // iterate over tables, running drop queries, and pushing a new promise to promise array
+    // for (const table in keyObject) {
+    //   await generateAndRunDropQuery(table);
+    // }
   },
   /** Expand to see detailed comments
    * Iterating over the passed in keyObject to remove the primaryKeyColumn and all foreignKeyColumns from table
-   * 'ALTER TABLE planets DROP COLUMN _id CASCADE, DROP COLUMN foreignKeyColumnName, DROP COLUMN foreignKeyColumnName;
+   * 'ALTER TABLE planets DROP Column _id int, ADD COLUMN foreignKeyColumnName INT, ADD COLUMN foreignKeyColumnName INT;
    *          keyObject = {
    *             tableName: {
    *                 primaryKeyColumns: {
@@ -279,51 +284,80 @@ myobj = {
     // define helper function to generate and run query
     const generateAndRunAddQuery = (table: string) => {
       let queryString = `ALTER TABLE ${table}`;
-      let count: number = 2;
+      // let count: number = 0;
+      const primaryKeyColumnName = Object.keys(
+        keyObject[table].primaryKeyColumns
+      )[0];
+      queryString += ` ADD COLUMN ${primaryKeyColumnName} INT`;
 
-      for (const pkc in keyObject[table].primaryKeyColumns) {
-        if (count > 2) queryString += ',';
-        queryString += ` ADD COLUMN ${pkc} INT`;
-        count += 1;
-      }
-      for (const fkc in keyObject[table].foreignKeyColumns) {
-        if (count > 2) queryString += ',';
-        queryString += ` ADD COLUMN ${fkc} INT`;
-        count += 1;
-      }
+      // for (const pkc in keyObject[table].primaryKeyColumns) {
+      //   if (count > 0) queryString += ',';
+      //   queryString += ` ADD COLUMN ${pkc} INT`;
+      //   count += 1;
+      // }
+
+      // for (const fkc in keyObject[table].foreignKeyColumns) {
+      //   if (count > 0) queryString += ',';
+      //   queryString += ` ADD COLUMN ${fkc} INT`;
+      //   count += 1;
+      // }
+
+      Object.keys(keyObject[table].foreignKeyColumns).forEach(
+        (foreignKeyColumnName) => {
+          // if (count > 0) queryString += ',';
+          queryString += `, ADD COLUMN ${foreignKeyColumnName} INT`;
+          // count += 1;
+        }
+      );
+
       queryString += ';';
 
       return Promise.resolve(pool.query(queryString));
     };
 
     // iterate over tables, running drop queries, and pushing a new promise to promise array
-    for (const table in keyObject) {
-      await generateAndRunAddQuery(table);
+
+    const arrayOfObj = Object.keys(keyObject);
+    for (let i = 0; i < arrayOfObj.length; i += 1) {
+      await generateAndRunAddQuery(i);
     }
+    // for (const table in keyObject) {
+    //   await generateAndRunAddQuery(table);{}
+    // }
   },
-  // initialize a new promise; we resolve this promise at the end of the last async function within the promise
+  /**
+   *
+   */
   getSchemaLayout: () =>
+    // initialize a new promise; we resolve this promise at the end of the last async function within the promise
     new Promise((resolve) => {
       const schemaLayout: any = {
         tableNames: [],
-        tables: {
-          // tableName: [columnObj array]
-        },
+        // tableName: [columnObj array]
+        tables: {},
       };
       pool
         // This query returns the names of all the tables in the database
         .query(
           "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name;"
         )
-        // then we save the table names into the schemaLayout object in the tableNames property
         .then((tables) => {
-          for (let i = 0; i < tables.rows.length; i += 1) {
-            schemaLayout.tableNames.push(tables.rows[i].table_name);
-          }
           const promiseArray: any = [];
-          for (const tableName of schemaLayout.tableNames) {
-            promiseArray.push(getColumnObjects(tableName));
+
+          // then we save the table names into the schemaLayout object in the tableNames property
+          for (let i = 0; i < tables.rows.length; i += 1) {
+            const tableName = tables.rows[i].table_name;
+            schemaLayout.tableNames.push(tableName);
           }
+          schemaLayout.tableNames.forEach((tableName) => {
+            promiseArray.push(getColumnObjects(tableName));
+          });
+
+          // Delete if working properly
+          // for (const tableName of schemaLayout.tableNames) {
+          //   promiseArray.push(getColumnObjects(tableName));
+          // }
+
           // we resolve all of the promises for the data info, and are returned an array of column data objects
           Promise.all(promiseArray).then((columnInfo) => {
             // here, we create a key for each table name and assign the array of column objects to the corresponding table name
