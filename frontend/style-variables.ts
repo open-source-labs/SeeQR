89e8ac546248028603c6382e190a1c1f180d432a
claudiohbsantos <claudiohbsantos@gmail.com>
2021-02-04 14:55:12 -0500

    General frontend Infrastructure

--- frontend/style-variables.ts
+++ frontend/style-variables.ts
@@ -2,3 +2,27 @@
  * This file contains all reusable variables for styling in styled-components
  * Individual variables should be named exports
  */
+
+ export const bgColor = '#2b2d35'
+ export const textColor = '#c6d2d5'
+
+// @import url('https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+// @import url('https://fonts.googleapis.com/css2?family=PT+Mono&display=swap');
+
+// // typography
+// $font-stack: 'PT Sans', sans-serif;
+// $font-input: 'PT Mono', monospace;
+// $p-weight: 100;
+// $title-weight: 300;
+// $default-text: 1em;
+
+// // colors
+// $background-modal-darkmode: #30353a;
+// $background-lightmode: #9abacc;
+// $primary-color-lightmode: #1a1a1a;
+// $primary-color-darkmode: #c6d2d5;
+// $border-darkmode: #444c50;
+// $button-darkmode: #596368;
+// $background-darkmode-darker: #292a30;
+// $mint-green: #6cbba9;
+
