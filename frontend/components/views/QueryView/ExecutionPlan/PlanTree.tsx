17a8fbef7c68238522cdb5358c0fde69979fabc9
claudiohbsantos <claudiohbsantos@gmail.com>
2021-02-11 00:00:15 -0500

    Basic unstyled plan tree

---
+++ frontend/components/views/QueryView/ExecutionPlan/PlanTree.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Tree, TreeNode } from 'react-organizational-chart';
+
+import PlanCard from './PlanCard';
+import { PlanNode, ExplainJson } from '../../../../types';
+
+interface PlanTreeNodeProps {
+  plan: PlanNode;
+}
+
+const PlanTreeNode = ({ plan }: PlanTreeNodeProps) => (
+  <TreeNode label={<PlanCard plan={plan} />}>
+    {plan.Plans?.map((child) => (
+      <PlanTreeNode plan={child} />
+    ))}
+  </TreeNode>
+);
+
+interface PlanTreeProps {
+  data: ExplainJson;
+}
+
+const PlanTree = ({ data }: PlanTreeProps) => (
+  <Tree
+    lineWidth="2px"
+    lineColor="black"
+    lineBorderRadius="10px"
+    label={<PlanCard plan={data.Plan} />}
+  >
+    {data?.Plan?.Plans?.map((plan) => (
+      <PlanTreeNode plan={plan} />
+    ))}
+  </Tree>
+);
+
+export default PlanTree;
--- frontend/components/views/QueryView/QueryPlan.tsx
+++ frontend/components/views/QueryView/QueryPlan.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import {Paper} from '@material-ui/core'
+import styled from 'styled-components';
 import { QueryData } from '../../../types';
+
+import PlanTree from './ExecutionPlan/PlanTree';
+
+const TreeContainer = styled.div`
+  padding: 20px;
+`;
 
 interface QueryPlanProps {
   executionPlan: QueryData['executionPlan'];
 }
 
 const QueryPlan = ({ executionPlan }: QueryPlanProps) => {
-  return <>{executionPlan?.toString() || 'Plan'}</>;
+  if (!executionPlan) return null;
+  return (
+    <TreeContainer>
+      <PlanTree data={executionPlan} />
+    </TreeContainer>
+  );
 };
 
 export default QueryPlan;
