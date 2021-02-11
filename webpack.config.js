7e99f58dec43783b6cd63cf42472b35ab0a5fe58
claudiohbsantos <claudiohbsantos@gmail.com>
2021-02-11 01:08:32 -0500

    Use Roboto Font

--- webpack.config.js
+++ webpack.config.js
@@ -65,6 +65,18 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
     ],
   },
   resolve: {
