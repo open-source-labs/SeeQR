16f78763e6bbb9f6814469578f93854980b80834
Cindy Chau <cindychau38@gmail.com>
2021-02-21 20:00:29 -0500

    Changed code to accomodate for dummy data modal

--- frontend/components/sidebar/DbList.tsx
+++ frontend/components/sidebar/DbList.tsx
@@ -104,7 +104,6 @@ const DbList = ({ selectedDb, setSelectedDb, show }: DbListProps) => {
           />
         ) : null}
       </StyledSidebarList>
-      {/* Validate Db name doesnt exist */}
       <AddNewDbModal
         open={openAdd}
         onClose={handleCloseAdd}
--- frontend/components/views/DbView/DbView.tsx
+++ frontend/components/views/DbView/DbView.tsx
@@ -1,9 +1,11 @@
 import { IpcMainEvent } from 'electron';
 import React, { useState, useEffect } from 'react';
+import { Button } from '@material-ui/core';
 import { AppState, isDbLists, DatabaseInfo, TableInfo } from '../../../types';
 import TablesTabs from './TablesTabBar';
 import DatabaseDetails from './DatabaseDetails';
 import { once } from '../../../lib/utils';
+import DummyDataModal from '../../modal/DummyDataModal';
 
 const { ipcRenderer } = window.require('electron');
 
@@ -19,6 +21,7 @@ const DbView = ({ selectedDb, show }: DbViewProps) => {
   const [dbTables, setTables] = useState<TableInfo[]>([]);
   const [selectedTable, setSelectedTable] = useState<TableInfo>();
   const [databases, setDatabases] = useState<DatabaseInfo[]>([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     // Listen to backend for updates to list of tables on current db
@@ -35,16 +38,35 @@ const DbView = ({ selectedDb, show }: DbViewProps) => {
     return () => ipcRenderer.removeListener('db-lists', tablesFromBackend);
   });
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const db = databases.find((dbN) => dbN.db_name === selectedDb);
+
   if (!show) return null;
   return (
     <>
-      <DatabaseDetails
-        db={databases.find((db) => db.db_name === selectedDb)}
-      />
+      <DatabaseDetails db={db} />
       <br />
       <TablesTabs
         tables={dbTables}
         selectTable={(table: TableInfo) => setSelectedTable(table)}
+      />
+      <br />
+      <br />
+      <Button variant="contained" color="primary" onClick={handleClickOpen}>
+        Generate Dummy Data
+      </Button>
+      <DummyDataModal
+        open={open}
+        onClose={handleClose}
+        dbName={db?.db_name}
+        tableName={selectedTable?.table_name}
       />
     </>
   );
