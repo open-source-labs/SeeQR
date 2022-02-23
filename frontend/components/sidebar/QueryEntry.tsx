80350dfe29619646ea4267ae79a66dd51b08a9e5
mimiwrp <wirapa.boon@gmail.com>
2022-02-22 17:10:58 -0800

    fixing drop-down componant with mui
    Co-authored-by: Eric Han eric.j.h92@gmail.com

--- frontend/components/sidebar/QueryEntry.tsx
+++ frontend/components/sidebar/QueryEntry.tsx
@@ -14,14 +14,19 @@ import {
 } from '../../style-variables';
 import { QueryData } from '../../types';
 import QueryDbname from './QueryDbname';
-import { Accordion } from 'react-bootstrap-accordion';
+import { deleteQuery, setCompare, key as queryKey } from '../../lib/queries';
+/******************* ACCORDION ***********************/
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const QueryText = styled(StyledListItemText)`
   & .MuiListItemText-secondary {
     color: ${textColor};
   }
 `;
-
 const CompareCheck = styled(Checkbox)`
   color: ${textColor};
 `;
@@ -44,24 +49,37 @@ const QueryEntry = ({
   deleteThisQuery,
 }: QueryEntryProps) => (
   <SidebarListItem button $customSelected={isSelected} onClick={select}>
-    <QueryText primary={query.label} />
+    {/* <QueryText primary={query.label} /> */}
 
     <ListItemSecondaryAction>
       <Tooltip title="drop down">
         {/* <CompareCheck onChange={setComparison} checked={isCompared} /> */}
-        <div className="accordion" id="accordionPanelsStayOpenExample"></div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="panelsStayOpen-headingOne">
-            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-              Accordion Item #1
-            </button>
-          </h2>
-          <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-            <div className="accordion-body">
-              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
+        <div>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>
+                <QueryText primary={query.label} />
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {/* //if statement here  */}
+                <QueryDbname
+                  query={query}
+                  isSelected={isSelected}
+                  select={select}
+                  setComparison={setComparison}
+                  isCompared={isCompared}
+                  deleteThisQuery={deleteThisQuery}
+                />
+                test
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
         </div>
       </Tooltip>
       <Tooltip title="Forget Query">
