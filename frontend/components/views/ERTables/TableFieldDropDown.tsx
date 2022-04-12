49c6e97987e5e76c9d6af3558de67d5850efae91
Jake Bradbeer <jakebradbeer@gmail.com>
2022-04-11 20:32:36 -0700

    refactored code

--- frontend/components/views/ERTables/TableFieldDropDown.tsx
+++ frontend/components/views/ERTables/TableFieldDropDown.tsx
@@ -5,20 +5,21 @@ type TableFieldDropDownProps = {
   idName: string;
   options: string[];
   defaultValue: string[] | string | number | boolean;
+  isDisabled?: boolean;
 }
 
 
 const TableFieldDropDown = (props: TableFieldDropDownProps) => {
-  const {label, idName, options, defaultValue} = props
+  const {label, idName, options, defaultValue, isDisabled} = props
   const optionsArray = options.map(opt => {
      const isSelected = (opt === defaultValue)
     return <option key={opt} selected={isSelected} value={opt}>{opt}</option>
   });
 
   return (
-    <div id={idName} className='field-info-dropdown'>
-      {label + ':'}
-      <select> 
+    <div id={`${idName}-wrapper`} className='field-info-dropdown'>
+      {`${label}:`}
+      <select id={idName} disabled={isDisabled}> 
         {optionsArray}
       </select> 
     </div>
