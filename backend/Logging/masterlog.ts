e6a0b8ac947497fa3514ea5f2104abd3f5ba7daa
RadiantGH <iangrapeo@gmail.com>
2022-07-22 12:20:30 -0700

    Logger now exports a function by default.

--- backend/Logging/masterlog.ts
+++ backend/Logging/masterlog.ts
@@ -12,49 +12,47 @@ const saveLogMessage = (message) => {
 }
 
 //Export
-const logger: Logger = {
-    log(message: string, logType: LogType = LogType.NORMAL) {
-        //Code for the log color
-        let colorCode = 0;
+const logger = function(message: string, logType: LogType = LogType.NORMAL) {
+    //Code for the log color
+    let colorCode = 0;
 
-        //Prefix for the message
-        let logString: any = logType;
+    //Prefix for the message
+    let logString: any = logType;
 
-        switch(logType) {
-            case LogType.ERROR: //RED
-                colorCode = 31;
-                break;
-            
-            case LogType.SUCCESS: //GREEN
-                colorCode = 32;
-                break;
-            
-            case LogType.WARNING: //YELLOW
-                colorCode = 33;
-                break;
-            
-            case LogType.SEND: //BLUE
-                colorCode = 34;
-                break;
+    switch(logType) {
+        case LogType.ERROR: //RED
+            colorCode = 31;
+            break;
+        
+        case LogType.SUCCESS: //GREEN
+            colorCode = 32;
+            break;
+        
+        case LogType.WARNING: //YELLOW
+            colorCode = 33;
+            break;
+        
+        case LogType.SEND: //BLUE
+            colorCode = 34;
+            break;
 
-            case LogType.SEND: //MAGENTA
-                colorCode = 35;
-                break;
+        case LogType.SEND: //MAGENTA
+            colorCode = 35;
+            break;
 
-            case LogType.NORMAL: //WHITE (And change logType string)
-                colorCode = 0;
-                logString = 'LOG';
-                break;
+        case LogType.NORMAL: //WHITE (And change logType string)
+            colorCode = 0;
+            logString = 'LOG';
+            break;
 
-            default:
-                colorCode = 0;
-                break;
-        }
-
-        console.log( `\u001b[1;${colorCode}m ${`[${logType}] ${message}`}` );
-        resetLogColor();
-        saveLogMessage(`[${logType}] ${message}`);
+        default:
+            colorCode = 0;
+            break;
     }
+
+    console.log( `\u001b[1;${colorCode}m ${`[${logType}] ${message}`}` );
+    resetLogColor();
+    saveLogMessage(`[${logType}] ${message}`);
 }
   
 export default logger;
