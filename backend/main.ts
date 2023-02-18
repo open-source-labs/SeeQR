b1b0c576450c13ee87756bb2d4bdac6db7821293
ayamoosa <ayamoosa98@gmail.com>
2023-02-18 11:07:16 -0800

    commit before audit

--- backend/main.ts
+++ backend/main.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { FamilyRestroomRounded } from '@mui/icons-material';
 import { BrowserWindow, Menu } from 'electron';
 
 const { app } = require('electron');
