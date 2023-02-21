165c36edd8d144821aa19e4e5a29614ea4e64e7c
utyvert <utkarshuppal@gmail.com>
2023-02-21 11:49:02 -0800

    Revert "confirm deletion dialog added"
    This reverts commit fe0bef9e36c1704b86b37cac29751a1fbcc56f89.

--- frontend/components/sidebar/DbEntry.tsx
+++ frontend/components/sidebar/DbEntry.tsx
@@ -1,14 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   IconButton,
   ListItemSecondaryAction,
   Tooltip,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-  Button,
 } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
@@ -26,7 +20,6 @@ interface DbEntryProps {
   duplicate: () => void;
   dbType: DBType;
 }
-
 const DbEntry = ({
   db,
   isSelected,
@@ -34,14 +27,11 @@ const DbEntry = ({
   duplicate,
   dbType,
 }: DbEntryProps) => {
-  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
-
   const handleDelete = () => {
     ipcRenderer
       .invoke('drop-db', db, isSelected, dbType)
       .then(() => {
         if (isSelected) select('', dbType);
-        setIsDeleteDialogOpen(false);
       })
       .catch(() =>
         sendFeedback({ type: 'error', message: `Failed to delete ${db}` })
@@ -62,36 +52,10 @@ const DbEntry = ({
           </IconButton>
         </Tooltip>
         <Tooltip title="Drop Database">
-          <IconButton edge="end" onClick={() => setIsDeleteDialogOpen(true)}>
+          <IconButton edge="end" onClick={handleDelete}>
             <DeleteIcon />
           </IconButton>
         </Tooltip>
-        <Dialog
-          open={isDeleteDialogOpen}
-          onClose={() => setIsDeleteDialogOpen(false)}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle style={{ color: 'black' }} id="alert-dialog-title">
-            Confirm deletion
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Are you sure you want to delete the database {db}?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button
-              onClick={() => setIsDeleteDialogOpen(false)}
-              color="primary"
-            >
-              Cancel
-            </Button>
-            <Button onClick={handleDelete} color="primary" autoFocus>
-              Delete
-            </Button>
-          </DialogActions>
-        </Dialog>
       </ListItemSecondaryAction>
     </SidebarListItem>
   );
