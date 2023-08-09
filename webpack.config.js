ba332d78727b93c5d843eb36ae87422872b741e4
Derek <dk.derek.koh@gmail.com>
2023-08-09 18:54:31 -0500

    minor changes while working on tests

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,11 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
             plugins: [
               isDevelopment && require.resolve('react-refresh/babel'),
             ].filter(Boolean),
