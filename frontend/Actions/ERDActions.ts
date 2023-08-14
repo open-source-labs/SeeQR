6ac503eb517e592ea10e8cf2caf20987141589d3
Derek <dk.derek.koh@gmail.com>
2023-08-14 17:14:11 -0500

    reorganized reducers and created expected types for ERD actions.

--- frontend/Actions/ERDActions.ts
+++
@@ -1,8 +0,0 @@
-type ERDActions =
-  | { type: 'ADD_TABLE' }
-  | { type: 'SAVE_CHANGES' }
-  | { type: 'EDIT_TABLE' }
-  | { type: 'SELECT_TABLE' }
-  | { type: 'GENERATE_DATA' };
-
-export default ERDActions;
