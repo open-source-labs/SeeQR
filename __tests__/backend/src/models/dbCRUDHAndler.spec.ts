e9534b59148cbef2ee9390397b2b30d75734ccd5
cyliang93 <cathyluong93@yahoo.com>
2024-02-02 18:40:18 -0800

    ##Description: fixed testing syntax/bug
    ##Changes Made
    
    ##Issues
    
    ##Co-authored-by
    Co-authored-by: Elaine Wong <ewong1217@gmail.com>
    Co-authored-by: Zoren Labrador <zorenzal@gmail.com>
    Co-authored-by: Kevin Chou <choukevin@berkeley.edu>

--- __tests__/backend/src/models/dbCRUDHAndler.spec.ts
+++ __tests__/backend/src/models/dbCRUDHAndler.spec.ts
@@ -1,68 +1,78 @@
+import 'text-encoding';
+import { BackendObjType } from '../../../../shared/types/dbTypes'; 
+import { DBType } from '../../../../backend/BE_types';
+import { erTableSchemaUpdate } from '../../../../backend/src/ipcHandlers/handlers/dbCRUDHandler';
+
+
+
+jest.mock('../../../../backend/src/utils/ertable-functions.ts', () => ({
+  backendObjToQuery: jest.fn(),
+}));
+
 describe('dbCRUDHandler tests', () => {
-  // setBaseConnections
+
   describe('erTableSchemaUpdate tests', () => {
-    test('it should only receive backendObj as parameter', () => {});
-    test('it should send async started back to frontend', () => {});
-    test(
-      'it should send backendObj to helper function to receive a queryString and a dbType back as query',
-    );
-    test(
-      'it should use query.queryString and query.dbType to run queryModel.query',
-    );
+      test('it should receive an event, backendObj, dbtype, dbName as parameter', async () => {
+       
+        const event = { sender: { send: jest.fn() } };
+        const backendObj: BackendObjType  = {
+          database: 'tester2',
+          updates: {
+            addTables: [
+              {
+                is_insertable_into: 'yes',
+                table_name: 'NewTable8',
+                table_schema: 'public',
+                table_catalog: 'tester2',
+                columns: [],
+              },
+            ],
+  
+            dropTables: [
+              {
+                table_name: 'newtable5',
+                table_schema: 'public',
+              },
+            ],
+  
+            alterTables: [
+              {
+                is_insertable_into: null,
+                table_catalog: 'tester2',
+                table_name: 'newtable7',
+                new_table_name: null,
+                table_schema: 'public',
+                addColumns: [],
+                dropColumns: [],
+                alterColumns: [],
+              },
+              {
+                is_insertable_into: null,
+                table_catalog: 'tester2',
+                table_name: 'newtable7',
+                new_table_name: null,
+                table_schema: 'puclic',
+                addColumns: [],
+                dropColumns: [],
+                alterColumns: [],
+              },
+            ],
+          },
+        };
+        const dbName = 'tester2';
+        const dbType = DBType.Postgres; 
+        await erTableSchemaUpdate(event, backendObj, dbName, dbType);
+        expect(event.sender.send).toHaveBeenCalledWith('async-started');
+      });
+    });
+
+    test('it should send backendObj to helper function to receive a queryString and a dbType back as query', () => {
+
+    });
+
+    test('it should use query.queryString and query.dbType to run queryModel.query', () => {
+
+    });
+
   });
-});
-
-describe('ertable-functions tests', () => {
-  describe('erdObjToQuery tests', () => {
-    test('it should only receive backendObj as parameter', () => {});
-    test('it should create an empty array', () => {});
-    test('it should identity the erdDbType from dbState', () => {});
-    test('it should use erdDbType to pick an appropriate query function for backendObj to act on and receive a query bacj', () => {});
-    test('it should return a query string');
-  });
-});
-
-/*
-const backendObj = {
-    database: 'tester2',
-    updates: {
-     addTables: [
-      {
-       is_insertable_into: 'yes',
-       table_name: 'NewTable8',
-       table_schema: 'puclic',
-       table_catalog: 'tester2',
-       columns: []
-      }
-     ],
-     
-     dropTables: [{
-      table_name: 'newtable5',
-      table_schema: 'puclic'
-      }
-     ],
-
-     alterTables: [
-      {
-        is_insertable_into: null,
-        table_catalog: 'tester2',
-        table_name: 'newtable7',
-        new_table_name: null,
-        table_schema: 'puclic',
-        addColumns: [Array],
-        dropColumns: [],
-        alterColumns: []
-      },
-      {
-        is_insertable_into: null,
-        table_catalog: 'tester2',
-        table_name: 'newtable7',
-        new_table_name: null,
-        table_schema: 'puclic',
-        addColumns: [Array],
-        dropColumns: [],
-        alterColumns: []
-      }]
-    }
-}
-*/
+  
---
+++ __tests__/backend/src/models/erTableFunc.spec.ts
@@ -0,0 +1,10 @@
+describe('ertable-functions tests', () => {
+    describe('erdObjToQuery tests', () => {
+      test('it should only receive backendObj as parameter', () => {});
+      test('it should create an empty array', () => {});
+      test('it should identity the erdDbType from dbState', () => {});
+      test('it should use erdDbType to pick an appropriate query function for backendObj to act on and receive a query bacj', () => {});
+      test('it should return a query string');
+    });
+  });
+  
