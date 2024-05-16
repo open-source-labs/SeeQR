42f744613bdadbbf6aa4b6163a0f8a98cfdc490b
michaelma7 <michaelma16688@gmail.com>
2024-05-15 20:41:34 -0400

    updating configs since require deprecated on several dependencies

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-// import { render } from 'react-dom';
 import { createRoot } from 'react-dom/client';
-
 import App from './components/App';
-// import 'codemirror/lib/codemirror.css';
-
 import '@fontsource/roboto';
 
-const root = createRoot(document.createElement('div'));
-root.id = 'root';
-document.body.appendChild(root);
+const doc = document.createElement('div') as HTMLElement;
+doc.id = 'root';
+const element = createRoot(doc);
+document.body.appendChild(doc);
 
-root.render(<App />);
+element.render(<App />);
 
 // const container = document.getElementById('root') as HTMLElement;
 // const root = createRoot(container);
