9474273762630d9a2a77cbd9f9bf4b1d2ca206a3
Michael Ma <122073103+michaelma7@users.noreply.github.com>
2024-05-24 11:36:58 -0400

    Update store.ts

--- frontend/state_management/store.ts
+++ frontend/state_management/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import appViewReducer from './Slices/AppViewSlice';
 import menuReducer from './Slices/MenuSlice';
 import queryReducer from './Slices/QuerySlice';
+import { useDispatch } from 'react-redux';
 
 // Configure the Redux store with slices for app view, menu, and query state
 const store = configureStore({
@@ -12,8 +13,11 @@ const store = configureStore({
   },
 });
 
+// exporting type of State for use in other files
 export type RootState = ReturnType<typeof store.getState>;
+// exporting dispatch type & useDispatch method to use in component files
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = useDipatch.withTypes<AppDispatch>();
 
 export default store;
 
